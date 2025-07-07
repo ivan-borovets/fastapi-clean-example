352f5a451bf58b91e5617e4df7aab18735a07945
ivan-borovets <130386813+ivan-borovets@users.noreply.github.com>
2025-07-08 01:49:40 +0500

    Add tests for password hasher

--- tests/performance/profile_password_hasher_bcrypt.py
+++ tests/performance/profile_password_hasher_bcrypt.py
@@ -7,35 +7,21 @@ from app.infrastructure.adapters.password_hasher_bcrypt import (
 )
 
 
-def run_operations(hasher: BcryptPasswordHasher) -> None:
+def profile_password_hashing(hasher: BcryptPasswordHasher) -> None:
     raw_password = RawPassword("raw_password")
     hashed = hasher.hash(raw_password)
     hasher.verify(raw_password=raw_password, hashed_password=hashed)
 
 
-def create_hasher() -> BcryptPasswordHasher:
-    pepper = PasswordPepper("Cayenne!")
-    return BcryptPasswordHasher(pepper)
-
-
-def create_profiler() -> LineProfiler:
-    profiler = LineProfiler()
-    profiler.add_function(run_operations)
-    return profiler
-
-
-def run_profiling(
-    profiler: LineProfiler,
-    hasher: BcryptPasswordHasher,
-) -> None:
-    profiler.runcall(run_operations, hasher)  # type: ignore[no-untyped-call]
-    profiler.print_stats()
-
-
 def main() -> None:
-    hasher = create_hasher()
-    profiler = create_profiler()
-    run_profiling(profiler, hasher)
+    pepper = PasswordPepper("Cayenne!")
+    hasher = BcryptPasswordHasher(pepper)
+
+    profiler = LineProfiler()
+    profiler.add_function(profile_password_hashing)
+
+    profiler.runcall(profile_password_hashing, hasher)  # type: ignore[no-untyped-call]
+    profiler.print_stats()
 
 
 if __name__ == "__main__":
