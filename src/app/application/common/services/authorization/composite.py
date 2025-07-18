8533d5f581e2b754f3996f0d6c4f1a1bb34cb06a
ivan-borovets <130386813+ivan-borovets@users.noreply.github.com>
2025-07-18 23:57:00 +0500

    WIP: remove all-of composite [mypy-incompatible]

--- src/app/application/common/services/authorization/composite.py
+++ src/app/application/common/services/authorization/composite.py
@@ -10,11 +10,3 @@ class AnyOf[PC: PermissionContext](Permission[PC]):
 
     def is_satisfied_by(self, context: PC) -> bool:
         return any(p.is_satisfied_by(context) for p in self._permissions)
-
-
-class AllOf[PC: PermissionContext](Permission[PC]):
-    def __init__(self, *permissions: Permission[PC]) -> None:
-        self._permissions = permissions
-
-    def is_satisfied_by(self, context: PC) -> bool:
-        return all(p.is_satisfied_by(context) for p in self._permissions)
--- tests/app/unit/application/test_composite.py
+++ tests/app/unit/application/test_composite.py
@@ -1,4 +1,4 @@
-from app.application.common.services.authorization.composite import AllOf, AnyOf
+from app.application.common.services.authorization.composite import AnyOf
 from tests.app.unit.application.permission_stubs import (
     AlwaysAllow,
     AlwaysDeny,
@@ -16,21 +16,6 @@ def test_any_of_denies_if_all_deny() -> None:
     assert not sut.is_satisfied_by(DummyContext())
 
 
-def test_all_of_allows_if_all_allow() -> None:
-    sut = AllOf(AlwaysAllow(), AlwaysAllow())
-    assert sut.is_satisfied_by(DummyContext())
-
-
-def test_all_of_denies_if_at_least_one_denies() -> None:
-    sut = AllOf(AlwaysAllow(), AlwaysDeny())
-    assert not sut.is_satisfied_by(DummyContext())
-
-
 def test_any_of_empty_returns_false() -> None:
-    sut = AnyOf()
+    sut: AnyOf[DummyContext] = AnyOf()
     assert not sut.is_satisfied_by(DummyContext())
-
-
-def test_all_of_empty_returns_true() -> None:
-    sut = AllOf()
-    assert sut.is_satisfied_by(DummyContext())
