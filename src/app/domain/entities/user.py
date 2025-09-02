be68169f8bf7e0773c4366a867fbec0872563398
ivan-borovets <130386813+ivan-borovets@users.noreply.github.com>
2025-09-03 01:48:06 +0500

    Explain domain layer better

--- src/app/domain/entities/user.py
+++ src/app/domain/entities/user.py
@@ -1,22 +1,3 @@
-"""
-- In the early stages of development
-  when the domain model is not yet clearly defined,
-  it is wiser to keep entities
-  flat (non-nested) and anemic (without behavior).
-  Their behavior resides in separate domain services,
-  even though this weakens encapsulation.
-
-- Once the core logic is well established,
-  some entities can, as aggregate roots,
-  become non-flat and rich (with behavior).
-  This best enforces invariants
-  but can be tricky to design once and for all.
-
-- Prefer rich value objects early,
-  freeing entities and services
-  from an excessive burden of local rules.
-"""
-
 from dataclasses import dataclass
 
 from app.domain.entities.base import Entity
