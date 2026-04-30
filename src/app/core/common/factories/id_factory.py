9906d4d480d07fdbf61f9828ea97166e0a28b231
ivan-borovets <130386813+ivan-borovets@users.noreply.github.com>
2026-04-30 23:01:42 +0300

    Simplify factory

--- src/app/core/common/factories/id_factory.py
+++ src/app/core/common/factories/id_factory.py
@@ -1,9 +1,7 @@
-from uuid import UUID
-
 from uuid_utils import compat as uuid_utils
 
 from app.core.common.entities.types_ import UserId
 
 
-def create_user_id(value: UUID | None = None) -> UserId:
-    return UserId(value if value is not None else uuid_utils.uuid7())
+def create_user_id() -> UserId:
+    return UserId(uuid_utils.uuid7())
